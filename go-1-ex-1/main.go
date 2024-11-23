36d897dd9db4ce9de18bb1b0ce5339bc6ebc5213
Patrick Bucher <patrick.bucher@sluz.ch>
2024-11-23 12:21:46 +0100

    exercise 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,7 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Patrick"
+	var lastName string = "Bucher"
+
+	dayOfBirth := 24
+	monthOfBirth := 6
+	yearOfBirth := 1987
+
+	var numberOfSiblings = 1
+
+	heightInMeters := 1.88
+
+	var zodiacSign rune = '\u264b'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
